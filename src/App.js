8e2b04180cdd6f9c7a62fc581866b286cf36d405
sneha2999 <athawalesneha8@gmail.com>
2024-02-27 20:45:58 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SelectLocation from './SelectLocation';
+
+function App() {
+  return (
+    <div className="App">
+      <SelectLocation/>
+    </div>
+  );
+}
+
+export default App;
